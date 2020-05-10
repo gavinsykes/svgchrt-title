3e7d7c0c91e347f12758ff3e1f2040eaad59836d
Gavin Sykes <gavin@gavinsykes.uk>
2020-05-10 14:33:14 +0100

    Adding webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  devtool : 'inline-source-map',
+  entry   : './src/index.ts',
+  mode    : 'production',
+  module  : {
+    rules : [
+      {
+        test    : /\.tsx?$/,
+        use     : 'ts-loader'
+      }
+    ]
+  },
+  output  : {
+    filename : 'main.js',
+    path     : path.resolve(__dirname, 'dist')
+  },
+  resolve : {
+    extensions : [ '.tsx' , '.ts' , '.js' ]
+  }
+};
